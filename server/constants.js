04c0cb6e4f85300f2aa06b774fbca30195fe735a
Andrew Faulds <ajf@ajf.me>
2012-12-30 12:52:28 +0000

    Fixed Origin

--- server/constants.js
+++ server/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
     DEBUG_MODE: (process.argv.hasOwnProperty('2') && process.argv[2] === '--debug'),
     DEFAULT_PORT: 9002,
-    DEFAULT_ORIGIN: 'http://ponyplace.ajf.me',
+    DEFAULT_ORIGIN: 'http://schnitzelverse.ajf.me',
     DEBUG_ORIGIN: 'http://localhost:8000',
     SITE_NAME: 'schnitzelVerse'
 };
