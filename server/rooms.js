9787960c1dfa915723d91b319967a85e4532794f
Andrew Faulds <ajf@ajf.me>
2013-01-06 00:14:06 +0000

    Room list sorting by user count

--- server/rooms.js
+++ server/rooms.js
@@ -85,6 +85,9 @@ var Rooms = {
                 });
             }
         }
+        list.sort(function (room1, room2) {
+            return room2.user_count - room1.user_count;
+        });
         return list;
     },
     doRoomChange: function (roomName, user) {
